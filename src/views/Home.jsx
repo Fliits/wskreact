1b9eb088c46dd1595d08841f9d8be8eb0f1ea1f7
Fliits <aatuska576@gmail.com>
2026-04-13 14:23:38 +0300

    feat: update media item URLs in Home component for consistency

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
     {
       media_id: 8,
       user_id: 5,
-      filename: 'https://place-hold.it/1200x800.jpg&text=Pic1&fontsize=120',
-      thumbnail: 'http://place-hold.it/320/240.jpg&text=Thumb2&fontsize=20',
+      filename: 'http://placehold.co/600x400/AAAAAA/FFFFFF/png',
+      thumbnail: 'http://placehold.co/600x400/AAAAAA/FFFFFF/png',
       filesize: 170469,
       media_type: 'image/jpeg',
       title: 'Picture 1',
@@ -19,8 +19,8 @@ const Home = () => {
     {
       media_id: 9,
       user_id: 7,
-      filename: 'https://place-hold.it/800x600.jpg&text=Pic2&fontsize=72',
-      thumbnail: 'http://place-hold.it/320/240.jpg&text=Thumb3&fontsize=20',
+      filename: 'http://placehold.co/600x400/BBBBBB/FFFFFF/png',
+      thumbnail: 'http://placehold.co/600x400/BBBBBB/FFFFFF/png',
       filesize: 1002912,
       media_type: 'image/jpeg',
       title: 'Pic 2',
@@ -32,7 +32,7 @@ const Home = () => {
       user_id: 2,
       filename:
         'http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4',
-      thumbnail: 'http://place-hold.it/320/240.jpg&text=Thumb1&fontsize=20',
+      thumbnail: 'http://placehold.co/600x400/000000/FFFFFF/png',
       filesize: 1236616,
       media_type: 'video/mp4',
       title: 'Bunny',
